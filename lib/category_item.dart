75fc9d2c1e725c8814d31fa00d2967638d2da6a7
Jitgitbit <rowowolf@gmail.com>
2020-05-19 21:06:36 +0200

    using InkWell !

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -8,22 +8,27 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: ,
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+          child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
