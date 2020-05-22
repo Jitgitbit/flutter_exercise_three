b3f9ed2fe2286d1898a4d15ec49ab6d1f2d4ba59
Jitgitbit <rowowolf@gmail.com>
2020-05-22 13:18:33 +0200

    better arborescence

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_exercise_three/category_item.dart';
 
-import './dummy_data.dart';
+import '../widgets/category_item.dart';
+
+import '../dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
