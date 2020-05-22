318ec49699eb838cc9ee6c8aac77c348cc2796a7
Jitgitbit <rowowolf@gmail.com>
2020-05-22 11:48:48 +0200

    Adding a Meal Model & Data

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
+
 enum Complexity {
   Simple,
   Challenging,
   Hard
+}
+enum Affordability {
+  Affordable,
+  Pricey,
+  Luxurious
 }
 
 class Meal {
@@ -13,4 +20,25 @@ class Meal {
   final List<String> steps;
   final int duration;
   final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  Meal({
+    @required this.id,
+    @required this.categories,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.ingredients,
+    @required this.steps,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+    @required this.isGlutenFree,
+    @required this.isLactoseFree,
+    @required this.isVegan,
+    @required this.isVegetarian,
+  });
 }
