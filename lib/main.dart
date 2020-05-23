d442c0e4db60ad5b188ab9548815b0bb9f44a5ca
Jitgitbit <rowowolf@gmail.com>
2020-05-23 15:50:18 +0200

    Navigating to the Meal Detail Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 
@@ -33,7 +34,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',                 //---> default is '/' , can be usefull to change in some cases!
       routes: {
         '/': (ctx) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()       //-------> less error-prone !!
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),     //-------> less error-prone !!
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
