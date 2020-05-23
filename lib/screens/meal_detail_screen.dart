536cdca0af80e374b09b587e6a7eebb6fc064a45
Jitgitbit <rowowolf@gmail.com>
2020-05-23 20:20:50 +0200

    DART method        .firstWhere

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+
     return Scaffold(
       appBar: AppBar(title: Text('$mealId')),
           body: Center(
